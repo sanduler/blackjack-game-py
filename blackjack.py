a841c07797b8e552175a458164cfc9597bd64dbc
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-24 12:13:27 -0800

    finished the naming scheme

--- blackjack.py
+++ blackjack.py
@@ -21,7 +21,7 @@ def deal_card():
     return card
 
 
-def calculate_score(cards):
+def calc_score(cards):
     """Take a list of cards and return the score calculated from the cards"""
 
     if sum(cards) == 21 and len(cards) == 2:
@@ -60,31 +60,31 @@ def play_game():
 
     user_cards = []
     computer_cards = []
-    is_game_over = False
+    game_over = False
 
     for i in range(2):
         user_cards.append(deal_card())
         computer_cards.append(deal_card())
 
-    while not is_game_over:
-        user_score = calculate_score(user_cards)
-        computer_score = calculate_score(computer_cards)
+    while not game_over:
+        user_score = calc_score(user_cards)
+        computer_score = calc_score(computer_cards)
         print(f"   Your cards: {user_cards}, current score: {user_score}")
         print(f"   Computer's first card: {computer_cards[0]}")
 
         if user_score == 0 or computer_score == 0 or user_score > 21:
-            is_game_over = True
+            game_over = True
         else:
             user_should_deal = input(
                 "Type 'y' to get another card, type 'n' to pass: ")
             if user_should_deal == "y":
                 user_cards.append(deal_card())
             else:
-                is_game_over = True
+                game_over = True
 
     while computer_score != 0 and computer_score < 17:
         computer_cards.append(deal_card())
-        computer_score = calculate_score(computer_cards)
+        computer_score = calc_score(computer_cards)
 
     print(f"   Your final hand: {user_cards}, final score: {user_score}")
     print(
