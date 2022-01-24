ac615adb6ca9ee4a2e8f96d921cff708602cec4d
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-24 09:23:47 -0800

    cleaned up the code further

--- blackjack.py
+++ blackjack.py
@@ -37,8 +37,10 @@ def cal_score(cards):
 
 user_score = cal_score(user_cards)
 computer_score = cal_score(computer_cards)
-print(f"    You cards: {user_cards}, current score: {user_score}")
+
+print(f"    Your cards: {user_cards}, current score: {user_score}")
 print(f"    Computer's first card: {computer_cards[0]}")
+
 if user_cards == 0 or computer_score == 0 or user_score > 21:
     is_game_over = True
 
